423eedcd3ecd54d7ee940917b49602f1fde9c701
Maxim Derbenev <dereban25@gmail.com>
2023-06-11 16:59:13 -0400

    Update gke_cluster.tf

--- gke_cluster.tf
+++ gke_cluster.tf
@@ -5,7 +5,7 @@ module "gke_cluster" {
   GKE_NUM_NODES    = 1
   GKE_MACHINE_TYPE = "e2-medium"
   DISK_SIZE_GB     = 20
-  GKE_CLUSTER_NAME = "maks-test"
+  GKE_CLUSTER_NAME = "main"
 }
 
 resource "null_resource" "write_gke_context_to_file" {
@@ -19,4 +19,25 @@ resource "null_resource" "add_fluxcd_helm" {
   provisioner "local-exec" {
     command = "helm repo add fluxcd-community https://fluxcd-community.github.io/helm-charts"
   }
+}
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
 }
